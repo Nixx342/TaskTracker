455ea833c10fb3c4acca30ca44be7dd71c2b8a31
Maksim Sytnik <sytnik.m.yu@gmail.com>
2025-01-19 19:57:42 +0500

    2

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,128 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import './App.css'
+// import LoginPage from "./assets/Pages/LoginPage.tsx";
+// import HomePage from "./assets/Pages/HomePage.tsx";
+// import {UserType} from './assets/Types/UserType.ts'
+//
+// function App() {
+//
+//     const registerUser =(user: UserType): void => {
+//         const openRequest = indexedDB.open('TaskTracker',1)
+//         openRequest.onerror = () => {
+//             console.error("Error: ", openRequest.error)
+//         }
+//         openRequest.onupgradeneeded = (event) => {
+//             const db = event.target.result;
+//             if (!db.objectStoreNames.contains('users')) {
+//                 db.createObjectStore('users', { keyPath: 'id' });
+//             }
+//         };
+//         openRequest.onsuccess = ()=> {
+//             const db = openRequest.result
+//
+//             db.createObjectStore('users',{keyPath: 'id'})
+//             const transaction = db.transaction('users', 'readwrite')
+//             const users = transaction.objectStore('users')
+//             const request = users.add(user)
+//             request.onsuccess = () => {
+//                 console.log('Пользователь ', user.username, ' успешно добавлен')
+//             }
+//             request.onerror = () => {
+//                 console.error('Error: ', request.error)
+//             }
+//         }
+//     }
+//
+//   return (
+//     <>
+//         <button onClick={()=>registerUser({id:Date.now(),username:'testUser',password:'123'})}>add</button>
+//         <LoginPage />
+//         <HomePage />
+//     </>
+//   )
+// }
+//
+// export default App
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+
+
+
+import './App.css';
+import LoginPage from "./assets/Pages/LoginPage.tsx";
+import HomePage from "./assets/Pages/HomePage.tsx";
+import { UserType } from './assets/Types/UserType.ts';
+import {useEffect, useState} from "react";
+
+
+function openDatabase(): Promise<IDBDatabase> {
+    return new Promise((resolve, reject): void => {
+        const request: IDBOpenDBRequest = indexedDB.open("TaskTracker", 1)
+        request.onupgradeneeded = (event: IDBVersionChangeEvent): void => {
+            const db: IDBDatabase = (event.target as IDBOpenDBRequest).result
+            const objectStore: IDBObjectStore = db.createObjectStore("users", { keyPath: "id" })
+            objectStore.createIndex("username", "username", { unique: false })
+            objectStore.createIndex("password", "password", { unique: false })
+            console.log("Хранилище users создано")
+        };
+        request.onsuccess = (event: Event): void => {
+            const db: IDBDatabase = (event.target as IDBOpenDBRequest).result
+            resolve(db)
+        };
+        request.onerror = (event: Event): void => {
+            reject((event.target as IDBOpenDBRequest).error)
+        };
+    });
 }
 
-export default App
+
+
+function App() {
+    const [db, setDb] = useState<IDBDatabase | null>(null);
+    useEffect(():() => void => {
+        openDatabase()
+            .then((database: IDBDatabase):void => {
+                setDb(database)
+                console.log("БД успешно подключена")
+            })
+            .catch((err: Error): void => {
+                console.error("Ошибка при подключении к БД: ",err);
+            })
+        return (): void => {
+            if (db) {
+                db.close()
+                console.log("Соединение с БД закрыто")
+            }
+        }
+    }, [])
+
+    const registerUser = (user: UserType): void => {
+        try {
+            const transaction = db.transaction("users", "readwrite")
+            const objectStore: IDBObjectStore = transaction.objectStore("users");
+            const addRequest = objectStore.add(user)
+
+            addRequest.onsuccess = (): void => {
+
+                console.log(`Пользователь "${user.username}" успешно добавлена`)
+            }
+            transaction.oncomplete = (): void => {
+                console.log('Транзакция успешно завершена')
+            }
+            transaction.onerror = (): void => {
+                console.error('Ошибка при выполнении транзакции: ',transaction.error)
+            }
+        } catch (err) {
+            console.error('Ошибка при добавлении Пользователя:', err)
+        }
+    }
+
+    return (
+        <>
+            <button onClick={() => registerUser({id: 'qwe' ,username: 'testUser', password: '123' })}>add</button>
+            <LoginPage />
+            <HomePage />
+        </>
+    );
+}
+
+export default App;
---
+++ frontend/src/assets/Types/UserType.ts
@@ -0,0 +1,5 @@
+ export interface UserType {
+     id: any
+     username: string
+     password: string
+ }
