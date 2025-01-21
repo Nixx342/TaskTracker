f790b34531f96c4093352b7c544a07ff6bc26f3f
Maksim Sytnik <sytnik.m.yu@gmail.com>
2025-01-22 00:25:33 +0500

    Перенос на ноут

--- frontend/src/assets/Pages/LoginPage.tsx
+++ frontend/src/assets/Pages/LoginPage.tsx
@@ -1,39 +1,68 @@
 import {useState} from "react";
 import { UserType } from '../Types/UserType.ts';
+import '../Styles/LoginPage.scss'
 
 const LoginPage = (props) => {
+// const LoginPage = (props: LoginPageType) => {
     const [username, setUsername] = useState<string>('');
     const [password, setPassword] = useState<string>('');
-    const [errorMsg, setErrorMsg] = useState<boolean>(false)
+    const [errorMsg, setErrorMsg] = useState<string>('')
 
     const modalOpen = (id: string) => {
         const modal: HTMLElement | null = document.getElementById(id)
         modal?.showModal()
+        setErrorMsg('')
     }
     const modalClose = (id: string) => {
         const modal: HTMLElement | null = document.getElementById(id)
         modal?.close()
+        setErrorMsg('')
     }
     const register =(): void => {
-
-        const user: UserType  = {
-            username: username,
-            password: password,
+        setErrorMsg('')
+        if (username && password) {
+            const user: UserType  = {
+                username: username,
+                password: password,
+            }
+            props.register(user)
+            setUsername('')
+            setPassword('')
+            modalClose('register')
+        } else {
+            if (!username) {
+                setErrorMsg('Введите имя пользователя!')
+            }
+            if (!password) {
+                setErrorMsg('Введите пароль!')
+            }
+            if (!username && !password) {
+                setErrorMsg('Заполните все поля!')
+            }
         }
-        props.register(user)
-        setUsername('')
-        setPassword('')
-        modalClose('register')
     }
     const login = () => {
-        const user: UserType  = {
-            username: username,
-            password: password
+        setErrorMsg('')
+        if (username && password) {
+            const user: UserType  = {
+                username: username,
+                password: password
+            }
+            props.login(user)
+            setUsername('')
+            setPassword('')
+            modalClose('login')
+        } else {
+            if (!username) {
+                setErrorMsg('Введите имя пользователя!')
+            }
+            if (!password) {
+                setErrorMsg('Введите пароль!')
+            }
+            if (!username && !password) {
+                setErrorMsg('Заполните все поля!')
+            }
         }
-        props.login(user)
-        setUsername('')
-        setPassword('')
-        modalClose('login')
     }
 
     return (
@@ -42,30 +71,40 @@ const LoginPage = (props) => {
             <button onClick={() => modalOpen('register')}>Зарегистрироваться</button>
 
             <dialog id="login">
+                <div className={'main-modal-block'}>
+                    <div className={'input-area'}>
+                        <span>Имя пользователя:</span>
+                        <input
+                            type={'text'}
+                            value={username}
+                            onChange={e => setUsername(e.target.value)}
+                        />
+                    </div>
+                    <div className={'input-area'}>
+                        <span>Пароль:</span>
+                        <input
+                            type={'password'}
+                            value={password}
+                            onChange={e => setPassword(e.target.value)}
+                        />
+                    </div>
+                </div>
+                <div className={'button-block'}>
+                    <button onClick={() => login()}>Войти</button>
+                    <button onClick={() => modalClose('login')}>Отмена</button>
+                </div>
                 {
                     errorMsg
-                        ? <span></span>
+                        ? <div className={`error-msg ${errorMsg ? 'visible' : ''}`}>{errorMsg}</div>
                         : null
                 }
-                <span>Введите имя пользователя:
-                    <input
-                        type={'text'}
-                        value={username}
-                        onChange={e => setUsername(e.target.value)}
-                    />
-                </span>
-                <span>Введите пароль:
-                    <input
-                        type={'password'}
-                        value={password}
-                        onChange={e => setPassword(e.target.value)}
-                    />
-                </span>
-                <button onClick={() => login()}>Войти</button>
-                <button onClick={() => modalClose('login')}>Отмена</button>
-
             </dialog>
             <dialog id="register">
+                {
+                    errorMsg
+                        ? <div className={'error-msg'}>{errorMsg}</div>
+                        : null
+                }
                 <span>Введите имя пользователя:
                     <input
                         type={'text'}
